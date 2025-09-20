7b622c46383b0cab534693d67cbbb4c87e734559
Rosemond Ampomah <amprosemond@gmail.com>
2025-09-20 23:17:56 +0000

    computer choice generated

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -3,10 +3,27 @@ const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
+let computerChoice;
 
 possibleChoices.forEach((possibleChoice) => {
   possibleChoice.addEventListener("click", (event) => {
     userChoice = event.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
   });
 });
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "scissors";
+  }
+  if (randomNumber === 1) {
+    computerChoice = "paper";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
